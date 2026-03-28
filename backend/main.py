7cc034a5a2d7c5a44d2394be2bbf454bcaa40eff
desmond andama <desmondandama@gmail.com>
2026-03-28 20:02:09 +0300

    These are added codes to our collab

--- backend/main.py
+++ backend/main.py
@@ -2,3 +2,11 @@ foods_in_kitchen = ["bread", "milk", "eggs", "cheese"]
 
 if "milk" in foods_in_kitchen:
     print("You have milk in the kitchen.")
+
+drinks_in_kitchen = ["juice", "soda", "water"]
+if "juice" in drinks_in_kitchen:
+    print("You have juice in the kitchen.")
+if "soda" in drinks_in_kitchen:
+    print("You have soda in the kitchen.")
+if "water" in drinks_in_kitchen:
+    print("You have water in the kitchen.")
